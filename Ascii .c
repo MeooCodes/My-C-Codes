a24379ca260a2736c31a110768605a9037a226c1
Mithila Debnath <2024000010176@seu.edu.bd>
2025-11-17 17:50:52 +0600

    Add files via upload

---
+++ Ascii .c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+int main() {
+
+    char ch ;
+
+    printf("Enter your value = ");
+    scanf("%c" , &ch);    // i wrote here %d which is wrong correct will be %c ---Here we need character("c") data type not integer  ("d") //
+    printf("ASCII value = %d \n",ch);
+
+
+
+
+
+    return 0  ;
+}
